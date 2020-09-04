26ed3ad30a9bad0f64004e552ff1f08244a47066
asapkyxy <70300375+asapkyxy@users.noreply.github.com>
2020-09-04 14:26:35 -0400

    Personal Composition 2

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function setup() {
+    
+    var red = "#f542bc";
+    var startX = 430;
+
+    createCanvas(800,600);
+    noStroke();
+
+    background("blue");
+
+    fill("red");
+    rect(100, 100, 300, 50);
+
+    fill(200,0,0);
+    circle( 100, 100, 30);
+
+    stroke(2);
+    line(0, 500, 200, 20);
+    
+
+}
